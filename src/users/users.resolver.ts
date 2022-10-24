5475e2cb3674d0a43f047933d0032033bcc47d3f
Tsunfire <underrated@gmail.com>
2022-10-24 16:12:24 -0700

    wip add user

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,16 +1,17 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UserCreateInput } from '../@generated/user/user-create.input';
+import { UserUpdateInput } from '../@generated/user/user-update.input';
+import { UserWhereUniqueInput } from '../@generated/user/user-where-unique.input';
+import { User } from '../@generated/user/user.model';
 import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Mutation(() => User)
-  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
-    return this.usersService.create(createUserInput);
+  createUser(@Args('data') data: UserCreateInput) {
+    return this.usersService.create(data);
   }
 
   @Query(() => [User], { name: 'users' })
@@ -19,17 +20,20 @@ export class UsersResolver {
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.usersService.findOne(id);
+  findOne(@Args('where') where: UserWhereUniqueInput) {
+    return this.usersService.findOne(where);
   }
 
   @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
+  updateUser(
+    @Args('where') where: UserWhereUniqueInput,
+    @Args('data') data: UserUpdateInput,
+  ) {
+    return this.usersService.update(where, data);
   }
 
   @Mutation(() => User)
-  removeUser(@Args('id', { type: () => Int }) id: number) {
-    return this.usersService.remove(id);
+  removeUser(@Args('where') where: UserWhereUniqueInput) {
+    return this.usersService.remove(where);
   }
 }
