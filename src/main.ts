aaaf7ce8b3ea49a2459f2c534ff52bf9403dcfab
soju <underrated@gmail.com>
2022-10-28 13:45:06 -0700

    ✨ add user (#2)
    * wip add user
    
    * update schema
    
    * Update serverless.yml
    
    * update seed
    
    * setup beanstalk deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT || 3000;
 
-  await app.listen(3001);
+  await app.listen(port);
 }
 bootstrap();
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(where: Prisma.UserWhereUniqueInput) {
+    return this.prisma.user.findUnique({ where });
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
+  update(where: Prisma.UserWhereUniqueInput, data: Prisma.UserUpdateInput) {
+    return this.prisma.user.update({ where, data });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(where: Prisma.UserWhereUniqueInput) {
+    return this.prisma.user.delete({ where });
   }
 }
